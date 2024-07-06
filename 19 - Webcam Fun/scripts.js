1116b37f75feaa2c1dbc6db3bd1057d8bc34cede
Callum <callum@clow.nz>
2024-07-06 15:59:41 +1200

    following along video tutorial for this one - more efficient learning this way

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,40 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+const getVideo = () => {
+	navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+	.then(localMediaStream => {
+		video.srcObject = localMediaStream;
+		video.play();
+	})
+	.catch(err => {
+		console.error('! ->' + err)
+	})
+}
+
+const paintToCanvas = () => {
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height)
+	}, 16)
+}
+
+const takePhoto = () => {
+	snap.currentTime = 0;
+	snap.play();
+
+	const data = canvas.toDataURL('image/jpeg');
+	const link = document.createElement('a')
+	link.href = data;
+	link.setAttribute('download', 'handsome')
+}
+
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas)
