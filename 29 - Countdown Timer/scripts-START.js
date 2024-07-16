abdf7bf542e9b68ce2438f9067f1b82fcf8d4b5b
Callum <callum@clow.nz>
2024-07-16 23:37:16 +1200

    I have quickly realised this method is far inferior and stupid to choose but it is a fun learning/practise exercise so I will see it through for the sake of educaitonal purposes

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,19 +1,28 @@
-const timerButtons = document.querySelectorAll('.timer__button')
-const timeLeft = document.querySelector('.display__time-left')
+const timerButtons = document.querySelectorAll(".timer__button");
+const timeLeft = document.querySelector(".display__time-left");
+let timer;
 
 const countDown = (time) => {
-	
-}
+  timeLeft.textContent = time;
+  console.log(time);
+};
+
+const stopTimer = () => {
+  while (timer--) {
+    window.clearTimeout(timer); // will do nothing if no timeout with id is present
+  }
+};
 
 const startTimer = (b) => {
-	button = b.target;
-	console.log(button)
-	const time = button.dataset.time;
+  stopTimer();
+  button = b.target;
+  const startTime = button.dataset.time;
 
-	timeLeft.textContent = time;
-	countDown(time);
-}
+  for (let i = startTime; i >= 0; i--) {
+    timer = setTimeout(() => countDown(i), (startTime - i) * 1000);
+  }
+};
 
-timerButtons.forEach(button => {
-	button.addEventListener('click', (b) => startTimer(b));
+timerButtons.forEach((button) => {
+  button.addEventListener("click", (b) => startTimer(b));
 });
